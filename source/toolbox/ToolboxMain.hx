c74987a564710aa73cb9b56a18db7738ef303fec
swordcube <49110074+swordcube@users.noreply.github.com>
2022-08-20 17:54:09 -0500

    time to think of an actually original idea for the toolbox

--- source/toolbox/ToolboxMain.hx
+++ source/toolbox/ToolboxMain.hx
@@ -1,22 +1,15 @@
 package toolbox;
 
 import flixel.FlxG;
-import flixel.ui.FlxButton;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import flixel.util.FlxSpriteUtil;
-import haxe.Json;
-import substates.ModSelectionMenu.PackJSON;
 import systems.MusicBeat;
 import systems.UIControls;
 
 class ToolboxMain extends MusicBeatState
 {
     var bg:FlxSprite;
-
-    var selectBTN:FlxButton;
-    var editBTN:FlxButton;
 
     override function create() {
         super.create();
@@ -27,50 +20,14 @@ class ToolboxMain extends MusicBeatState
         );
 
         FlxG.mouse.visible = true;
+        
+        // gonna atleast try to make an original ui for this menu
+        // i might need some assistance
 
-        bg = new FlxSprite().loadGraphic(FNFAssets.returnAsset(IMAGE, AssetPaths.image("menuBGGradient")));
-        add(bg);
-
-        // Dumbass Rounded Rect
-        var drr:FlxSprite = new FlxSprite().makeGraphic(700, 300, FlxColor.TRANSPARENT);
-        drr.screenCenter();
-        drr.alpha = 0.6;
-        add(drr);
-
-        var curModIcon:FlxSprite = new FlxSprite(drr.x + 20, drr.y + 20);
-        curModIcon.loadGraphic(FNFAssets.returnAsset(IMAGE, AssetPaths.asset("pack.png")));
-        curModIcon.setGraphicSize(100, 100);
-        curModIcon.updateHitbox();
-        add(curModIcon);
-
-        var json:PackJSON = Json.parse(FNFAssets.returnAsset(TEXT, AssetPaths.json("pack")));
-
-        var curModName:FlxText = new FlxText(curModIcon.x + (curModIcon.width + 10), curModIcon.y, drr.width - (curModIcon.width + 20));
-        curModName.text = json.name;
-        curModName.setFormat(AssetPaths.font("vcr"), 32);
-        add(curModName);
-
-        var curModDesc:FlxText = new FlxText(curModName.x, curModName.y + 40, drr.width - (curModIcon.width + 20));
-        curModDesc.text = json.desc;
-        curModDesc.setFormat(AssetPaths.font("vcr"), 24);
-        add(curModDesc);
-
-        selectBTN = new FlxButton(0, 0, "Select Mod", function() {
-            openSubState(new substates.ModSelectionMenu());
-        });
-        selectBTN.setPosition(drr.x + (drr.width - (selectBTN.width + 10)), drr.y + (drr.height - (selectBTN.height + 10)));
-        add(selectBTN);
-
-        if(!json.locked)
-        {
-            editBTN = new FlxButton(0, 0, "Edit Mod", function() {
-                Main.switchState(new toolbox.ToolboxEditor());
-            });
-            editBTN.setPosition(selectBTN.x - (selectBTN.width + 10), selectBTN.y);
-            add(editBTN);
-        }
-
-        FlxSpriteUtil.drawRoundRect(drr, 0, 0, drr.width, drr.height, 15, 15, FlxColor.BLACK);
+        var trolled:FlxText = new FlxText(0, 0, 0, "get trolled");
+        trolled.setFormat(AssetPaths.font("vcr"), 24);
+        trolled.setBorderStyle(OUTLINE, FlxColor.BLACK, 2);
+        add(trolled);
     }
 
     override function update(elapsed:Float) {
