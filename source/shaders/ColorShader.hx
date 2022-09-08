609f459d180ff3ea87119b70063e82437c697235
Raf <85767304+RafaelGiacom@users.noreply.github.com>
2022-09-08 20:56:21 -0300

    cool

--- source/shaders/ColorShader.hx
+++ source/shaders/ColorShader.hx
@@ -3,69 +3,17 @@ package shaders;
 class ColorShader extends FlxFixedShader {
     @:glFragmentSource('#pragma header
 
-        uniform float r;
-        uniform float g;
-        uniform float b;
+        uniform vec3 color;
         uniform bool enabled;
         
-        uniform bool blurEnabled;
-        uniform float x;
-        uniform float y;
-        uniform int passes;
-        
         void main() {
-            vec2 coordinates = openfl_TextureCoordv;
             vec4 finalColor = flixel_texture2D(bitmap, openfl_TextureCoordv);
             if (enabled) {
-                if (blurEnabled) {
-                    // real stuff
-                    float r = 0;
-                    float g = 0;
-                    float b = 0;
-                    float a = 0;
-                    float t = 0;
-            
-                    float realX = x / openfl_TextureSize.x;
-                    float realY = y / openfl_TextureSize.y;
-                    for (int i = -passes; i < passes; ++i) {
-                        vec4 color = flixel_texture2D(bitmap, vec2(openfl_TextureCoordv.x + (i * realX / passes), openfl_TextureCoordv.y + (i * realY / passes)));
-                        r += color.r;
-                        g += color.g;
-                        b += color.b;
-                        a += color.a;
-                        ++t;
-                    }
-            
-                    finalColor = vec4(r / t, g / t, b / t, a / t);
-                }
-
                 float diff = finalColor.r - ((finalColor.g + finalColor.b) / 2.0);
-                gl_FragColor = vec4(((finalColor.g + finalColor.b) / 2.0) + (r * diff), finalColor.g + (g * diff), finalColor.b + (b * diff), finalColor.a);
-                
+                gl_FragColor = vec4(((finalColor.g + finalColor.b) / 2.0) + (color.r/255 * diff), finalColor.g + (color.g/255 * diff), finalColor.b + (color.b/255 * diff), finalColor.a);
             } else {
-                if (blurEnabled) {
-                    // real stuff
-                    float r = 0;
-                    float g = 0;
-                    float b = 0;
-                    float a = 0;
-                    float t = 0;
-            
-                    float realX = x / openfl_TextureSize.x;
-                    float realY = y / openfl_TextureSize.y;
-                    for (int i = -passes; i < passes; ++i) {
-                        vec4 color = flixel_texture2D(bitmap, vec2(openfl_TextureCoordv.x + (i * realX / passes), openfl_TextureCoordv.y + (i * realY / passes)));
-                        r += color.r;
-                        g += color.g;
-                        b += color.b;
-                        a += color.a;
-                        ++t;
-                    }
-                    finalColor = vec4(r / t, g / t, b / t, a / t);
-                }
                 gl_FragColor = finalColor;
             }
-            
         }
     ')
     public function new(r:Int, g:Int, b:Int, motion_blur:Null<Bool> = null, passes:Int = 10) {
@@ -75,8 +23,6 @@ class ColorShader extends FlxFixedShader {
     }
 
     public function setColors(r:Int, g:Int, b:Int) {
-        this.r.value = [r / 255];
-        this.g.value = [g / 255];
-        this.b.value = [b / 255];
+        this.color.value = [r, g, b];
     }
 }
