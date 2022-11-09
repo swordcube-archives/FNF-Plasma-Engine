d925f2873a16d58eece8e8f07ea35722d976a4dd
Goofgoof43 <91701399+Goofgoof43@users.noreply.github.com>
2022-11-08 20:30:20 -0500

    Create dependicies.bat

---
+++ dependicies.bat
@@ -0,0 +1,5 @@
+haxelib git flixel https://github.com/YoshiCrafter29/flixel
+haxelib git hxCodec https://github.com/swordcube/hxCodec-testing
+haxelib git hscript-improved https://github.com/YoshiCrafter29/hscript-improved
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git openfl https://github.com/openfl/openfl
