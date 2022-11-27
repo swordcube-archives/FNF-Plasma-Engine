26a634e0f88942378004674c79df52712be50084
swordcube <swordsoftarchive@gmail.com>
2022-11-27 15:00:05 -0600

    tank stage + ugh & guns are functional, time for stress!!

--- src/funkin/substates/GameOverSubstate.hx
+++ src/funkin/substates/GameOverSubstate.hx
@@ -1,5 +1,8 @@
 package funkin.substates;
 
+import openfl.media.Sound;
+import funkin.scripting.events.SubStateCreationEvent;
+import funkin.scripting.Script;
 import funkin.states.PlayState;
 import funkin.game.Character;
 import flixel.FlxG;
@@ -21,55 +24,99 @@ class GameOverSubstate extends FNFSubState {
 		retrySound = Paths.sound('game/retrySound');
 	}
 
+	public var script:ScriptModule;
+	public var runDefaultCode:Bool = true;
+
+	public var randomGameover:Int = 1;
+	public var playingDeathSound:Bool = false;
+
+	public var insultSound:Sound;
+
+	var week7Songs:Array<String> = [
+		"ugh",
+		"guns",
+		"stress"
+	];
+
 	public function new(x:Float, y:Float, char:String = "bf-dead") {
 		super();
 
-		Conductor.position = 0;
+		var exclude:Array<Int> = [];
+		// if (prefs.get("censor-naughty"))
+		// 	exclude = [1, 3, 8, 13, 17, 21];
+		randomGameover = FlxG.random.int(1, 25, exclude);
 
-		bf = new Character(x, y, true).loadCharacter(char);
-		add(bf);
+		if(week7Songs.contains(PlayState.SONG.name.toLowerCase()))
+			insultSound = Assets.load(SOUND, Paths.sound('game/week7/jeffGameover/jeffGameover-' + randomGameover));
 
-		camFollow = new FlxObject(0, 0, 1, 1);
-		add(camFollow);
+		script = Script.load(Paths.script('data/substates/GameOverSubstate'));
+		var event = script.event("onSubStateCreation", new SubStateCreationEvent(this));
 
-		FlxG.sound.play(Assets.load(SOUND, deathSound));
-		Conductor.bpm = 100;
+		if(!event.cancelled) {
+			Conductor.position = 0;
 
-		FlxG.camera.target = null;
-		FlxG.camera.scroll.set();
+			bf = new Character(x, y, true).loadCharacter(char);
+			add(bf);
 
-		bf.playAnim('firstDeath');
+			camFollow = new FlxObject(0, 0, 1, 1);
+			add(camFollow);
+
+			FlxG.sound.play(Assets.load(SOUND, deathSound));
+			Conductor.bpm = 100;
+
+			FlxG.camera.target = null;
+			FlxG.camera.scroll.set();
+
+			bf.playAnim('firstDeath');
+		} else runDefaultCode = false;
 	}
 
 	override function update(elapsed:Float) {
+		script.call("onUpdate", [elapsed]);
+		script.call("update", [elapsed]);
+
 		super.update(elapsed);
 
-		if (controls.getP("ACCEPT")) {
-			endBullshit();
+		if(runDefaultCode) {
+			if (controls.getP("ACCEPT")) endBullshit();
+
+			if (controls.getP("BACK")) {
+				FlxG.sound.music.stop();
+
+				if (PlayState.isStoryMode)
+					FlxG.switchState(new funkin.states.menus.StoryMenuState());
+				else
+					FlxG.switchState(new funkin.states.menus.FreeplayState());
+			}
+
+			if (bf.animation.curAnim.name == 'firstDeath' && bf.animation.curAnim.curFrame == 12) {
+				camFollow.setPosition(bf.getGraphicMidpoint().x + bf.positionOffset.x, bf.getGraphicMidpoint().y + bf.positionOffset.y);
+				FlxG.camera.follow(camFollow, LOCKON, 0.01);
+			}
+
+			if(week7Songs.contains(PlayState.SONG.name.toLowerCase())) {
+				if(bf.animation.curAnim.name == 'firstDeath' && bf.animation.curAnim.finished && !playingDeathSound) {
+					bf.playAnim('deathLoop');
+					playingDeathSound = true;
+					coolStartDeath(0.2);
+					FlxG.sound.play(insultSound, 1, false, null, true, function() {
+						FlxG.sound.music.fadeIn(4, 0.2, 1);
+					});
+				}
+			} else if (bf.animation.curAnim.name == 'firstDeath' && bf.animation.curAnim.finished) {
+				bf.playAnim('deathLoop');
+				coolStartDeath();
+			}
+
+			if (FlxG.sound.music.playing) Conductor.position = FlxG.sound.music.time;
 		}
 
-		if (controls.getP("BACK")) {
-			FlxG.sound.music.stop();
+		script.call("onUpdatePost", [elapsed]);
+		script.call("updatePost", [elapsed]);
+	}
 
-			if (PlayState.isStoryMode)
-				FlxG.switchState(new funkin.states.menus.StoryMenuState());
-			else
-				FlxG.switchState(new funkin.states.menus.FreeplayState());
-		}
-
-		if (bf.animation.curAnim.name == 'firstDeath' && bf.animation.curAnim.curFrame == 12) {
-			camFollow.setPosition(bf.getGraphicMidpoint().x + bf.positionOffset.x, bf.getGraphicMidpoint().y + bf.positionOffset.y);
-			FlxG.camera.follow(camFollow, LOCKON, 0.01);
-		}
-
-		if (bf.animation.curAnim.name == 'firstDeath' && bf.animation.curAnim.finished) {
-			bf.playAnim('deathLoop');
-			FlxG.sound.playMusic(Assets.load(SOUND, deathMusic));
-		}
-
-		if (FlxG.sound.music.playing) {
-			Conductor.position = FlxG.sound.music.time;
-		}
+	function coolStartDeath(?startVol:Float = 1) {
+		FlxG.sound.playMusic(Assets.load(SOUND, deathMusic), startVol);
 	}
 
 	var isEnding:Bool = false;
