8c6337dd643e951766c557e362c88b53077ce1ec
swordcube <swordsoftarchive@gmail.com>
2022-12-04 13:37:26 -0600

    juhhjskjfdkljasfd [skip ci]

--- src/funkin/macros/BuildCounterMacro.hx
+++ src/funkin/macros/BuildCounterMacro.hx
@@ -1,9 +1,5 @@
 package funkin.macros;
 
-// haha i got this working fuck you
-// but it updates when the haxe vs code haxe extension
-// recompiles the game partially to update shit
-// hlep
 class BuildCounterMacro {
     public static macro function getBuildNumber() {
         // Doesn't work on release because we don't show the number there
