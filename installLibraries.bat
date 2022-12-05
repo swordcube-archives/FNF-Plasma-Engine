963d323c3b2f3c9e8267f07a66ca518215c49528
swordcube <swordsoftarchive@gmail.com>
2022-12-04 21:12:17 -0600

    update library installer shits [skip ci]

--- installLibraries.bat
+++ installLibraries.bat
@@ -5,5 +5,5 @@ haxelib git flixel https://github.com/Leather128/flixel
 haxelib git openfl https://github.com/openfl/openfl
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
-haxelib remove flixel 4.11.0
+haxelib remove flixel 5.0.0
 haxelib remove openfl 9.2.0
