cbc172b8b9a080dfd00783a8ca516d564490c099
swordcube <swordsoftarchive@gmail.com>
2022-12-12 18:31:27 -0600

    okay leather

--- installLibraries.bat
+++ installLibraries.bat
@@ -5,5 +5,3 @@ haxelib git flixel-leather https://github.com/Leather128/flixel
 haxelib git openfl https://github.com/openfl/openfl
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
-haxelib remove flixel 5.0.0
-haxelib remove openfl 9.2.0
